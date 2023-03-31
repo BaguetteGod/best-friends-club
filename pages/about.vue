7785e4d790d8f27abe9cb906ba77204efdb42212
BaguetteGod <mika.vanmilligen@student.hu.nl>
2023-03-31 20:50:44 +0200

    About page pics

--- pages/about.vue
+++ pages/about.vue
@@ -18,7 +18,7 @@
         </div>
 
         <div
-          class="text-5xl lg:text-8xl text-wrap max-w-2xl text-center h-fit lg:h-full flex flex-col lg:justify-between items-center p-4 lg:p-20"
+          class="text-5xl lg:text-6xl text-wrap max-w-2xl text-center h-fit lg:h-full flex flex-col lg:justify-between items-center p-4 lg:p-20"
         >
           <span>
             Ontmoet de leden die de Beste Vrienden Club zo iconisch maken.
@@ -37,8 +37,8 @@
       >
         <div class="w-full lg:w-3/5 overflow-hidden h-fit zoom-element">
           <div
-            id="image-members"
-            class="w-full scale-150 lg:scale-200 image-style image-scaling bg-cover bg-bottom"
+            id="image-sjebbel"
+            class="w-full scale-150 lg:scale-200 image-style image-scaling bg-cover bg-bottom md:bg-center"
           />
         </div>
 
@@ -53,9 +53,9 @@
             voor de goede dingen in het leven.
           </span>
 
-          <span class="pt-4 text-xl font-normal">
-            Dit zorgt voor een diverse mix van interessante en dynamische mensen
-            van onder andere nieuwe en gevestigde werelden.
+          <span class="pt-4 text-xl font-normal w-full">
+            "De definitie van Sybren is keer op keer hetzelfde proberen en
+            andere resultaten verwacthen."
           </span>
         </div>
       </div>
@@ -65,8 +65,8 @@
       <div class="flex flex-col-reverse lg:flex-row gap-12 h-fit lg:h-full">
         <div class="w-full lg:w-3/5 overflow-hidden h-fit zoom-element">
           <div
-            id="image-members"
-            class="w-full scale-150 lg:scale-200 image-style image-scaling bg-cover bg-bottom"
+            id="image-stini"
+            class="w-full scale-150 lg:scale-150 image-style image-scaling bg-cover bg-center"
           />
         </div>
 
@@ -76,14 +76,14 @@
           <span class="w-full">Stijn van den Munckhof</span>
 
           <span class="pt-10 lg:pt-20 text-3xl">
-            De leden van De Beste Vrienden Club bestaan uit een groep
-            gelijkgestemde, zorgvuldig geselecteerde mensen die een passie delen
-            voor de goede dingen in het leven.
+            Diversiteit en inclusiviteit zijn belangrijke aspecten binnen onze
+            prestigieuze club. Om deze reden hebben wij Stijn binnen de
+            organisatie een positie gegeven om deze waarden door te voeren tot
+            in de diepste groeven van onze bedrijfsvoering.
           </span>
 
-          <span class="pt-4 text-xl font-normal">
-            Dit zorgt voor een diverse mix van interessante en dynamische mensen
-            van onder andere nieuwe en gevestigde werelden.
+          <span class="pt-4 text-xl font-normal w-full">
+            "Ik ben er klaar mee."
           </span>
         </div>
       </div>
@@ -95,25 +95,24 @@
       >
         <div class="w-full lg:w-3/5 overflow-hidden h-fit zoom-element">
           <div
-            id="image-members"
-            class="w-full scale-150 lg:scale-200 image-style image-scaling bg-cover bg-bottom"
+            id="image-wjettnie"
+            class="w-full scale-150 lg:scale-200 image-style image-scaling bg-cover bg-top"
           />
         </div>
 
         <div
           class="text-5xl lg:text-8xl text-wrap w-full lg:w-2/5 text-left h-fit lg:h-full flex flex-col justify-center items-center p-4 lg:p-20"
         >
-          <span class="w-full">Wouter Boomert</span>
+          <span class="w-full">Wouter Bomert</span>
 
           <span class="pt-10 lg:pt-20 text-3xl">
-            De leden van De Beste Vrienden Club bestaan uit een groep
-            gelijkgestemde, zorgvuldig geselecteerde mensen die een passie delen
-            voor de goede dingen in het leven.
+            Mede-oprichter van de beste vrienden club. Daarnaast heeft Wouter de
+            FOCK - dimensies al jarenlang succesvol weten toe te passen bij de
+            werving van onze leden.
           </span>
 
-          <span class="pt-4 text-xl font-normal">
-            Dit zorgt voor een diverse mix van interessante en dynamische mensen
-            van onder andere nieuwe en gevestigde werelden.
+          <span class="pt-4 text-xl font-normal text-left w-full">
+            “Ik heb een eigen mening... toch Mika?”
           </span>
         </div>
       </div>
@@ -123,8 +122,8 @@
       <div class="flex flex-col-reverse lg:flex-row gap-12 h-fit lg:h-full">
         <div class="w-full lg:w-3/5 overflow-hidden h-fit zoom-element">
           <div
-            id="image-members"
-            class="w-full scale-150 lg:scale-200 image-style image-scaling bg-cover bg-bottom"
+            id="image-mekkle"
+            class="w-full scale-150 lg:scale-200 image-style image-scaling bg-cover bg-bottom md:bg-top"
           />
         </div>
 
@@ -134,14 +133,15 @@
           <span class="w-full">Mika van Milligen</span>
 
           <span class="pt-10 lg:pt-20 text-3xl">
-            De leden van De Beste Vrienden Club bestaan uit een groep
-            gelijkgestemde, zorgvuldig geselecteerde mensen die een passie delen
-            voor de goede dingen in het leven.
+            Wie kent hem niet? Ook wel als wildcard van de Beste Vrienden Club
+            genoemd. Deze mede-oprichter speelt een grote rol in de sfeer van de
+            club. Het zal je ook niet verbazen dat dit lid een gebruikelijke
+            Mika pullt, door onaangekondigd niet op te dagen tijdens
+            ledenvergaderingen.
           </span>
 
-          <span class="pt-4 text-xl font-normal">
-            Dit zorgt voor een diverse mix van interessante en dynamische mensen
-            van onder andere nieuwe en gevestigde werelden.
+          <span class="pt-4 text-xl font-normal w-full">
+            "Kanker! OH SORRY, NIET KANKER"
           </span>
         </div>
       </div>
